d3d4a8420bf2800c337d487414552d506fcdad6d
suthyscott <sutherlandscott21@gmail.com>
2023-01-03 18:21:46 -0700

    requested data from API

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Characters = () => {
+    const [characters, setCharacters] = useState([])
+
+    const getChars = () => {
+        axios.get('https://hp-api.onrender.com/api/characters')
+            .then(res => setCharacters(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(getChars, [])
+
   return (
     <div>Characters</div>
   )
--- src/components/Spells.js
+++ src/components/Spells.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Spells = () => {
+  const [spells, setSpells] = useState([])
+
+  const getSpells = () => {
+    axios.get('https://hp-api.onrender.com/api/spells')
+      .then(res => setSpells(res.data))
+      .catch(err => console.log(err))
+  }
+
+  useEffect(getSpells, [])
+  
   return (
     <div>Spells</div>
   )
